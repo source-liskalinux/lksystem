38f04181a94c186c3e6ebe770cdeed1163bed569
Janorovic Volkov 🦊🌸 <janorovicvolkov@gmail.com>
2026-08-28 18:48:49 +0700

    fastening a bit

--- src/bin/lksystem.rs
+++ src/bin/lksystem.rs
@@ -91,8 +91,8 @@ fn main() -> io::Result<()> {
         clear_console();
         ui::success(format!("System {action} signal received!"));
         ui::success("All services has been stopped!");
-        ui::log(format!("System will {action} in 3 seconds...."));
-        thread::sleep(Duration::from_secs(3));
+        ui::log(format!("System will {action}...."));
+        thread::sleep(Duration::from_secs(2));
         reboot(match action.as_str() {
             "reboot" => RB_AUTOBOOT,
             "halt" => RB_HALT_SYSTEM,
